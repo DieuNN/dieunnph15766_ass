ba4c4b967f2242b41af7713529a8545e75354d0d
nongngocdieu <nongngocdieu20122002@gmail.com>
2021-07-26 15:40:25 +0700

    Designed LauncherActivity.kt, Added AUTOINCREMENT into database

--- app/src/main/java/com/example/dieunnph15766_ass/activity/LauncherActivity.kt
+++ app/src/main/java/com/example/dieunnph15766_ass/activity/LauncherActivity.kt
@@ -1,6 +1,11 @@
 package com.example.dieunnph15766_ass.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.dieunnph15766_ass.R
 import com.example.dieunnph15766_ass.database.*
@@ -9,40 +14,16 @@ import com.example.dieunnph15766_ass.model.*
 class LauncherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
         setContentView(R.layout.activity_main)
 
-        //using Database Inspector to check whether I created database
-
-        val database = Database(this)
-        val userDB = UserDB(database)
-        val incomeDB = IncomeDB(database)
-        val outcomeDB = OutcomeDB(database)
-        val incomeTypeDB = IncomeTypeDB(database)
-        val outcomeTypeDB = OutcomeTypeDB(database)
-
-        var user = User(123, "Dieu", "Dep trai")
-        userDB.newUser(user)
-        userDB.getAllUsers()
-
-
-        var income = Income(123, "income name", "income date", "income type name", 123, 100)
-        incomeDB.newIncome(income)
-
-
-
-        var outcome = Outcome(123, "income name", "income date", "income type name", 123, 100)
-        outcomeDB.newOutcome(outcome)
-
-
-        var incomeType = IncomeType(123, "outcome type")
-        incomeTypeDB.newIncomeType(incomeType)
-
-
-
-        var outcomeType  = OutcomeType(123, "outcome type")
-        outcomeTypeDB.newOutcomeType(outcomeType)
-
-        //database created successfully, use Database Inspector to check
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, WelcomeActivity::class.java))
+            finish()
+        }, 3000)
 
     }
 }
---
+++ app/src/main/java/com/example/dieunnph15766_ass/activity/WelcomeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.dieunnph15766_ass.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dieunnph15766_ass.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+
+    }
+}
